8894ea9170c80c90fed2f4a559e6b9caaec8d37a
ramapriya0023 <priya.rama0023@gmail.com>
2024-10-18 21:41:47 +0530

    Handled multi length delimiters

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,12 +1,23 @@
 const StringCalculator = (() => {
   const extractDelimiter = (input) => {
+    let delimiter = ",";
+    let numbers = input;
+    // "//[##]\n2##-3##4"
     if (input.startsWith("//")) {
       const delimiterEndIndex = input.indexOf("\n");
-      const delimiter = input.substring(2, delimiterEndIndex);
-      const numbers = input.substring(delimiterEndIndex + 1);
-      return { delimiter, numbers };
+      const delimiterToIdentify = input.substring(2, delimiterEndIndex);
+
+      const delimiterMatch = delimiterToIdentify.match(/\[(.+?)\]/);
+      if (delimiterMatch) {
+        delimiter = delimiterMatch[1];
+      } else {
+        delimiter = delimiterToIdentify;
+      }
+
+      numbers = input.substring(delimiterEndIndex + 1);
     }
-    return { delimiter: ",", numbers: input };
+
+    return { delimiter, numbers };
   };
 
   const add = (input) => {
@@ -14,12 +25,13 @@ const StringCalculator = (() => {
 
     const { delimiter, numbers } = extractDelimiter(input);
 
-    const numberArray = numbers.split(new RegExp(`[${delimiter}\n]`));
+    const escapedDelimiter = delimiter.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&");
+    const numberArray = numbers.split(new RegExp(`${escapedDelimiter}|\n`));
 
     const negativeValues = [];
-
     const sum = numberArray.reduce((acc, curr) => {
       const num = parseInt(curr, 10);
+      if (isNaN(num)) return acc;
       if (num < 0) {
         negativeValues.push(num);
       }
