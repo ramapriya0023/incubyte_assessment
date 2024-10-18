11ab941dcc181af3f63ad2ba19842d0abb2674e6
ramapriya0023 <priya.rama0023@gmail.com>
2024-10-18 20:04:13 +0530

    Test case for handling newlines and commas

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -14,4 +14,22 @@ describe("StringCalculator basic add tests", () => {
     expect(StringCalculator.add("1,5")).toBe(6);
     expect(StringCalculator.add("2,3")).toBe(5);
   });
+
+  test("should return the sum of multiple comma-separated numbers", () => {
+    expect(StringCalculator.add("1,1,1,1,1,1,1")).toBe(7);
+    expect(StringCalculator.add("100,200,300,400")).toBe(1000);
+    expect(StringCalculator.add("0,0,0,0")).toBe(0);
+  });
+
+  test("should handle only newlines as separators", () => {
+    expect(StringCalculator.add("1\n2\n3")).toBe(6);
+    expect(StringCalculator.add("4\n5\n6")).toBe(15);
+    expect(StringCalculator.add("7\n8\n9\n10")).toBe(34);
+  });
+
+  test("should handle commas and newlines as separators", () => {
+    expect(StringCalculator.add("1\n2,3")).toBe(6);
+    expect(StringCalculator.add("4,5\n6")).toBe(15);
+    expect(StringCalculator.add("7,8\n9")).toBe(24);
+  });
 });
